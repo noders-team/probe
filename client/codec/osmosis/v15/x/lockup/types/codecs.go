06b8244e55a6bb310c810fc3538def2b4907821a
Artem Bogomaz <artembogomaz@gmail.com>
2024-07-23 12:04:59 +0700

    adding ethsecp256k1 codec

--- client/codec/osmosis/v15/x/lockup/types/codecs.go
+++ client/codec/osmosis/v15/x/lockup/types/codecs.go
@@ -8,10 +8,10 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 
-	"github.com/oracleNetworkProtocol/plugchain-sdk-go/crypto/keys/ed25519"
-	"github.com/oracleNetworkProtocol/plugchain-sdk-go/crypto/keys/ethsecp256k1"
-	"github.com/oracleNetworkProtocol/plugchain-sdk-go/crypto/keys/secp256k1"
-	cryptotypes "github.com/oracleNetworkProtocol/plugchain-sdk-go/crypto/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/evmos/ethermint/crypto/ethsecp256k1"
 )
 
 // RegisterLegacyAminoCodec registers concrete types on the codec.
--- client/encoding.go
+++ client/encoding.go
@@ -6,18 +6,18 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	"github.com/cosmos/cosmos-sdk/std"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
+	"github.com/evmos/ethermint/crypto/ethsecp256k1"
 	osmosisGammTypes "github.com/nodersteam/probe/client/codec/osmosis/v15/x/gamm/types"
 	osmosisLockupTypes "github.com/nodersteam/probe/client/codec/osmosis/v15/x/lockup/types"
 	osmosisPoolManagerTypes "github.com/nodersteam/probe/client/codec/osmosis/v15/x/poolmanager/types"
 	tendermintLiquidityTypes "github.com/nodersteam/probe/client/codec/tendermint/liquidity/x/liquidity/types"
-	"github.com/oracleNetworkProtocol/plugchain-sdk-go/crypto/keys/ed25519"
-	"github.com/oracleNetworkProtocol/plugchain-sdk-go/crypto/keys/ethsecp256k1"
-	"github.com/oracleNetworkProtocol/plugchain-sdk-go/crypto/keys/secp256k1"
-	cryptotypes "github.com/oracleNetworkProtocol/plugchain-sdk-go/crypto/types"
 )
 
 type Codec struct {
