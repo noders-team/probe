f18c9be26d0ee5d2dbda882fd8a09fd88b2130c7
Artem Bogomaz <artembogomaz@gmail.com>
2024-08-16 21:58:02 +0700

    fixing pagination

--- query/tx.go
+++ query/tx.go
@@ -5,28 +5,27 @@ package query
 import (
 	"fmt"
 
-	"github.com/cosmos/cosmos-sdk/types/query"
 	txTypes "github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/nodersteam/probe/client"
 )
 
-// TxRPC Get Transactions for the given block height.
+// TxsAtHeightRPC Get Transactions for the given block height.
 // Other query options can be specified with the GetTxsEventRequest.
 //
 // RPC endpoint is defined in cosmos-sdk: proto/cosmos/tx/v1beta1/service.proto,
 // See GetTxsEvent(GetTxsEventRequest) returns (GetTxsEventResponse)
-func TxsAtHeightRPC(q *Query, height int64, codec client.Codec) (*txTypes.GetTxsEventResponse, error) {
-	if q.Options.Pagination == nil {
-		pagination := &query.PageRequest{Limit: 100}
-		q.Options.Pagination = pagination
-	}
+func TxsAtHeightRPC(q *Query, height int64, codec client.Codec, page, limit uint64) (*txTypes.GetTxsEventResponse, error) {
 	orderBy := txTypes.OrderBy_ORDER_BY_UNSPECIFIED
 
-	req := &txTypes.GetTxsEventRequest{Events: []string{"tx.height=" + fmt.Sprintf("%d", height)}, Pagination: q.Options.Pagination, OrderBy: orderBy}
+	req := &txTypes.GetTxsEventRequest{
+		Events:  []string{"tx.height=" + fmt.Sprintf("%d", height)},
+		Page:    page,
+		Limit:   limit,
+		OrderBy: orderBy}
 	return TxsRPC(q, req, codec)
 }
 
-// TxRPC Get Transactions for the given block height.
+// TxsRPC Get Transactions for the given block height.
 // Other query options can be specified with the GetTxsEventRequest.
 //
 // RPC endpoint is defined in cosmos-sdk: proto/cosmos/tx/v1beta1/service.proto,
